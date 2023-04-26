71e092c1db46a3b9fe427b39d002928f02c3e390
Andre Carrera <andre@carrera.red>
2023-04-26 12:20:53 -0600

    added head and html

--- emails/index.tsx
+++ emails/index.tsx
@@ -1,36 +1,56 @@
-import { Button } from '@react-email/button';
+import {
+  Body,
+  Button,
+  Container,
+  Column,
+  Head,
+  Heading,
+  Html,
+  Img,
+  Preview,
+  Row,
+  Section,
+  Text,
+} from '@react-email/components';
+import * as React from 'react';
 import { Tailwind } from '@react-email/tailwind';
 
 export default () => {
   return (
-    <Tailwind
-      config={{
-        plugins: [require("daisyui")],
-        daisyui: {
-          styled: false,
-          themes: false,
-          base: false,
-          utils: false,
-          logs: false,
-          rtl: false,
-          prefix: "",
-          darkTheme: "dark",
-        },
-        theme: {
-          extend: {
-            colors: {
-              brand: '#007291',
+    <Html>
+      <Head>
+        <title>My email title</title>
+      </Head>
+      <Tailwind
+        config={{
+          plugins: [require("daisyui")],
+          corePlugins: { container: false },
+          daisyui: {
+            styled: true,
+            themes: false,
+            base: false,
+            utils: false,
+            logs: false,
+            rtl: false,
+            prefix: "",
+            darkTheme: "dark",
+          },
+          theme: {
+            extend: {
+              colors: {
+                brand: '#007291',
+              },
             },
           },
-        },
-      }}
-    >
-      <Button
-        href="https://example.com"
-        className="btn btn-primary"
+        }}
       >
-        Click me
-      </Button>
-    </Tailwind>
+        <Button
+          href="https://example.com"
+          className="btn btn-primary"
+        >
+          Click me
+        </Button>
+      </Tailwind>
+    </Html>
   );
 };
